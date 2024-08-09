61153b4952518297fd5dd23d08da097e11f582c0
unknown <richard@vanirinstalledsales.com>
2024-08-09 11:55:56 -0400

    styled

--- supervisor.js
+++ supervisor.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", async function () {
     const apiKey = 'pat6QyOfQCQ9InhK4.4b944a38ad4c503a6edd9361b2a6c1e7f02f216ff05605f7690d3adb12c94a3c';
     const baseId = 'app9gw2qxhGCmtJvW';
