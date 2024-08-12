6224c07b3a3f2ed09911440e2e1c0f021c9118e8
unknown <richard@vanirinstalledsales.com>
2024-08-12 10:55:50 -0400

    added arrow function time.html

--- timesheet.js
+++ timesheet.js
@@ -49,7 +49,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     console.log('DOM fully loaded and parsed');
     initializeTimeDropdowns();
-    initializeKeyboardNavigation();
 
     const apiKey = 'pat6QyOfQCQ9InhK4.4b944a38ad4c503a6edd9361b2a6c1e7f02f216ff05605f7690d3adb12c94a3c';
     const baseId = 'app9gw2qxhGCmtJvW';
