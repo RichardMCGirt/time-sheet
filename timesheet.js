59bedad050688620ebf08c3eba46bb769ff13f06
RichardMcGirt <richard.mcgirt@vanirinstalledsales.com>
2025-04-22 15:28:25 -0400

    t

--- timesheet.js
+++ timesheet.js
@@ -1740,7 +1740,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let popupsEnabled = true;
     
 
-    if (userEmail === 'diana.smith@vanirinstalledsales.com') {
+    if (userEmail === 'heath.kornegay@vanirinstalledsales.5com') {
         console.log('👀 Prank mode: Ads activated for Heath');
     
         const adMessages = [
